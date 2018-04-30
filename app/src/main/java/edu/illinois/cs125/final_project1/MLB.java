f2428ccd6b40753503c34e3fb1d2b530a29aa345
1Kishan <krt2@illinois.edu>
2018-04-30 00:55:41 -0500

    Added methods in NBA class for comparisons

--- app/src/main/java/edu/illinois/cs125/final_project1/MLB.java
+++ app/src/main/java/edu/illinois/cs125/final_project1/MLB.java
@@ -1,17 +1,11 @@
 package edu.illinois.cs125.final_project1;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class MLB extends AppCompatActivity {
     public TextView api;
@@ -39,30 +33,12 @@ public class MLB extends AppCompatActivity {
         });
 
 
-
     }
 
     public void launchMLBCompare() {
 
         Intent intent = new Intent(this, MLBcompare.class);
         startActivity(intent);
-    }
-
-    public int homeRuns(String json) {
-        JsonParser parser = new JsonParser();
-        JsonObject rootObj = parser.parse(json).getAsJsonObject();
-        JsonArray playerStatsArray = rootObj.getAsJsonArray("playerstatsentry");
-        for (JsonElement element : playerStatsArray) {
-            JsonObject elementObject = element.getAsJsonObject();
-            String elementString = elementObject.getAsString();
-            if (elementString.equals("stats")) {
-                JsonElement homeRunsElement = elementObject.get("Homeruns");
-                JsonObject homeRunsObject = homeRunsElement.getAsJsonObject();
-                return homeRunsObject.get("#text").getAsInt();
-
-            }
-        }
-        return 0;
     }
 
 }
--- app/src/main/java/edu/illinois/cs125/final_project1/NBA.java
+++ app/src/main/java/edu/illinois/cs125/final_project1/NBA.java
@@ -1,8 +1,7 @@
 package edu.illinois.cs125.final_project1;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -32,23 +31,58 @@ public class NBA extends AppCompatActivity {
             }
         });
 
-        Button nbaCompare = (Button) findViewById(R.id.compare_nba);
-
-        nbaCompare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchNBACompare();
-            }
-        });
-
-
 
     }
 
-    public void launchNBACompare() {
-
-        Intent intent = new Intent(this, NBAcompare.class);
-        startActivity(intent);
+    /**
+     * This method extracts the points per game for  a player
+     * @param json json string from the sportsfeed api
+     * @return return the points per game
+     */
+    public int getPPG(String json) {
+        if (json == null) {
+            return 0;
+        }
+        return 0;
     }
+
+    /**
+     * This method extracts the assists per game for  a player
+     * @param json json string from the sportsfeed api
+     * @return return the assists per game
+     */
+    public int getAPG(String json) {
+        return 0;
+    }
+
+    /**
+     * This method extracts the rebounds per game for  a player
+     * @param json json string from the sportsfeed api
+     * @return return the rebounds per game
+     */
+    public int getRPG(String json) {
+        return 0;
+    }
+
+    /**
+     * This method extracts the three point percentage per game for  a player
+     * @param json json string from the sportsfeed api
+     * @return return the 3 point fg % per game
+     */
+    public int getFg3PtPct(String json) {
+        return 0;
+    }
+
+    /**
+     * This method extracts the turnovers per game for  a player
+     * @param json json String from the sportsfeed api
+     * @return returns the turnovers per game for a player
+     */
+    public int getTovPerGame(String json) {
+        return 0;
+    }
+
+
+
 
 }
